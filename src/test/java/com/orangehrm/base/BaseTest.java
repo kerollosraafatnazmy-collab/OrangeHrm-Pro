795032d7b2b05c6ac22c638484e97ca61ff705e7
Kerollos Raafat <kerollosraafatnazmy@gmail.com>
2026-06-11 00:33:29 +0300

    Fix Script Bug

--- src/test/java/com/orangehrm/base/BaseTest.java
+++ src/test/java/com/orangehrm/base/BaseTest.java
@@ -24,7 +24,6 @@ public class BaseTest {
 
         System.out.println("✅ Base Setup Done!");
     }
-
     protected RequestSpecification getAuthSpec() {
         return new RequestSpecBuilder()
                 .addRequestSpecification(requestSpec)
