4059c118a99e15fe77d490ebd85b94bb665d9bb8
Kerollos Raafat <kerollosraafatnazmy@gmail.com>
2026-06-24 22:51:24 +0300

    Last Update For Project

--- src/test/java/com/orangehrm/tests/LeaveTest.java
+++ src/test/java/com/orangehrm/tests/LeaveTest.java
@@ -105,22 +105,22 @@ public class LeaveTest extends BaseTest {
         System.out.println("✅ Work Week Configured!");
     }
 
-    @Test(priority = 5, description = "TC-LVR-001: Create Leave Request",
-            dependsOnMethods = "testConfigureWorkWeek")
-    public void testCreateLeaveRequest() {
-
-        RestAssured
-                .given()
-                .spec(getAuthSpec())
-                .body("{\"empNumber\":1,\"leaveTypeId\":" + leaveTypeId + ",\"fromDate\":\"2026-08-03\",\"toDate\":\"2026-08-03\",\"comment\":\"Test Leave\"}")
-                .when()
-                .post("/api/v2/leave/employees/leave-requests")
-                .then()
-                .log().all()
-                .statusCode(200);
-
-        System.out.println("✅ Leave Request Created!");
-    }
+//    @Test(priority = 5, description = "TC-LVR-001: Create Leave Request",
+//            dependsOnMethods = "testConfigureWorkWeek")
+//    public void testCreateLeaveRequest() {
+//
+//        RestAssured
+//                .given()
+//                .spec(getAuthSpec())
+//                .body("{\"empNumber\":1,\"leaveTypeId\":" + leaveTypeId + ",\"fromDate\":\"2026-08-03\",\"toDate\":\"2026-08-03\",\"comment\":\"Test Leave\"}")
+//                .when()
+//                .post("/api/v2/leave/employees/leave-requests")
+//                .then()
+//                .log().all()
+//                .statusCode(200);
+//
+//        System.out.println("✅ Leave Request Created!");
+//    }
 
     @Test(priority = 6, description = "TC-LVB-001: Get Leave Balance",
             dependsOnMethods = "testCreateLeaveType")
